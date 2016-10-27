6d6a1ac26baf43302b4042b185fdf16f46e260df
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-10-27 18:37:22 +0000

    menuAssignment done

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
    def main_menu
      puts "Main Menu - #{address_book.entries.count} entries"
      puts "1 - View all entries"
-     puts "2 - Create an entry"
-     puts "3 - Search for an entry"
-     puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "2 - View Entry Number n"
+     puts "3 - Create an entry"
+     puts "4 - Search for an entry"
+     puts "5 - Import entries from a CSV"
+     puts "6 - Exit"
      print "Enter your selection: "
 
      selection = gets.to_i
@@ -24,17 +25,21 @@ class MenuController
          main_menu
        when 2
          system "clear"
-         create_entry
+         view_entry
          main_menu
        when 3
          system "clear"
-         search_entries
+         create_entry
          main_menu
        when 4
          system "clear"
-         read_csv
+         search_entries
          main_menu
        when 5
+         system "clear"
+         read_csv
+         main_menu
+       when 6
          puts "Good-bye!"
          exit(0)
        else
@@ -46,15 +51,43 @@ class MenuController
  
    def view_all_entries
       address_book.entries.each do |entry|
-      system "clear"
-      puts entry.to_s
-      entry_submenu(entry)
-     end
+         system "clear"
+         puts entry.to_s
+         entry_submenu(entry)
+      end
      
      system "clear"
      puts "End of entries"
    end
  
+   def view_entry
+      system "clear"
+      if (address_book.entries.length == 0 )
+         error_message( "No record available.  Please create at least one entry" )
+         main_menu
+      end
+      print "Entry#: "
+      entry = gets.chomp.to_i
+      if (entry == 0) || ( entry > address_book.entries.length )
+         error_message( "Record #{entry} does not exist" )
+         main_menu
+      end   
+      puts address_book.entries[entry-1].to_s
+      puts
+      puts "Press any key to continue"
+      gets
+      system "clear"
+   end
+   
+   def error_message ( message )
+      puts
+      puts message
+      puts
+      puts "Press any key to continue"
+      gets
+      system "clear"
+   end   
+   
    def create_entry
      system "clear"
      puts "New AddressBloc Entry"
