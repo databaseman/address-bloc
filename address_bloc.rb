70c5b7b7e3520baef40456826d06fd4320515787
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-10-27 16:43:48 +0000

    menuCheckpoint done

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,6 @@
-puts "Welcome to AddressBloc!"
+require_relative 'controllers/menu_controller'
+ 
+ menu = MenuController.new
+ system "clear"   # Clear the command line
+ puts "Welcome to AddressBloc!"
+ menu.main_menu 
